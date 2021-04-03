3b9c489b8257b5d3f7c9edbebea9653d94b95c53
Saeed-Afzal <65010428+Saeed-Afzal@users.noreply.github.com>
2021-04-03 23:15:47 +0500

    Add files via upload

---
+++ Class#32/app.js
@@ -0,0 +1,46 @@
+// var text = "    Lorem ipsum dolor sit amet consectetur, adipisicing elit. World War II Consequuntur pariatur id, modi sed reprehenderit, esse itaque vitae natus adipisci facilis temporibus earum quos ! numquam, deleniti quod. Animi adipisci sed ratione.World War II"
+
+// for(var i =0;i<text.length;i++){
+//     if(text.slice(i,i+12)==="World War II"){
+//         text = text.slice(0,i) + "The Second World War" + text.slice(i+12)
+//     }
+// }
+// document.write(text)
+
+// var text1 = prompt("Enter your text")
+// var text2 = prompt("What u want to replace?")
+// var text3 = prompt("Enter Word that replace previos text")
+// for(var i =0;i<text1.length;i++){
+//     if(text1.slice(i,i+5)=== text2){
+//         text = text1.slice(0,i) + text3 + text1.slice(i+5)
+//     }
+// }
+// document.write(text)
+
+
+
+// for (var i = 0; i < text.length; i++) {
+//     if (text.charAt(i) === "!") {
+//         alert("Exclamation point found!");
+//         break;
+//     }
+// }
+
+
+//Head n Tail
+
+// var user1 = prompt("Toss: Press 1 or 2")
+// var user2;
+
+// var bigDecimal = Math.random(); 
+//  var improvedNum = (bigDecimal * 2) + 1; 
+//   var numberOfStars = Math.floor(improvedNum);
+
+//   if(numberOfStars == user1 ){
+//     console.log("First User Win")
+
+//   }
+//   else{
+//       console.log("Second User Win")
+//   }
+
